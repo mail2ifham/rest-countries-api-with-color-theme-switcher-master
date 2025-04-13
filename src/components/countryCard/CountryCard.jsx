bd19c7e7ab0b0c43f142126514f212001487c9f0
ifham <mail2ifham@gmail.com>
2025-04-13 13:42:24 +0530

    refactor CountryCard component for improved structure and styling

--- src/components/countryCard/CountryCard.jsx
+++ src/components/countryCard/CountryCard.jsx
@@ -2,30 +2,39 @@ import React from "react";
 import "./countryCard.css";
 
 const CountryCard = ({ country }) => {
+  const sa = "skjhdgkd";
   return (
     <ul className="card-werpper">
-        <li key={country.cca2} className="card-container" >
+      <li key={country.cca2} className="card-container">
+        {country.flags && country.flags.alt ? (
           <img
             className="card-image"
-            src={country.flags.png}
             alt={country.flags.alt}
+            src={country.flags.png}
           />
-          <div className="card-detail-container">
-            <h3 className="card-header">{country.name.common}</h3>
-            <p className="card-details">
-              <b>Population:</b>
-              {` ${country.population.toLocaleString()}`}
-            </p>
-            <p className="card-details">
-              <b>Region:</b>
-              {` ${country.region}`}
-            </p>
-            <p className="card-details">
-              <b>Capital:</b>
-              {` ${country.capital}`}
-            </p>
-          </div>
-        </li>
+        ) : (
+          <img
+          className="card-image"
+          alt={"flag of " + country.name.common}
+          src={country.flags.png}
+        />
+        )}
+        <div className="card-detail-container">
+          <h1 className="card-header">{country.name.common}</h1>
+          <p className="card-details">
+            <b>Population:</b>
+            {` ${country.population.toLocaleString()}`}
+          </p>
+          <p className="card-details">
+            <b>Region:</b>
+            {` ${country.region}`}
+          </p>
+          <p className="card-details">
+            <b>Capital:</b>
+            {` ${country.capital}`}
+          </p>
+        </div>
+      </li>
     </ul>
   );
 };
